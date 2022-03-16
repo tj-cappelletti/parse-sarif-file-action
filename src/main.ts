dd9f319b5d205c1ce9e4c04df4ef326a61053f4a
TJ Cappelletti <tj-cappelletti@github.com>
2022-03-15 22:20:41 -0400

    Fixed linting commands
    Ran prettier to cleanup files

--- src/main.ts
+++ src/main.ts
@@ -1,78 +1,105 @@
 import * as core from '@actions/core'
-import { SarifParser } from './sarif-parser'
-
-function failWorkFlow() {
-
-}
+import {SarifParser} from './sarif-parser'
 
 async function run(): Promise<void> {
   try {
-    const failOnAnyString: string = core.getInput('failOnAny');
-    const failOnErrorsString: string = core.getInput('failOnErrors');
-    const failOnWarningsString: string = core.getInput('failOnWarnings');
-    const failOnNotesString: string = core.getInput('failOnNotes');
-    const jmesPathQuery: string = core.getInput('jmesPathQuery');
-    const sarifFilePath: string = core.getInput('sarifFile');
+    const failOnAnyString: string = core.getInput('failOnAny')
+    const failOnErrorsString: string = core.getInput('failOnErrors')
+    const failOnWarningsString: string = core.getInput('failOnWarnings')
+    const failOnNotesString: string = core.getInput('failOnNotes')
+    const jmesPathQuery: string = core.getInput('jmesPathQuery')
+    const sarifFilePath: string = core.getInput('sarifFile')
 
-    core.debug(`failOnAny: ${failOnAnyString}`);
-    core.debug(`failOnErrors: ${failOnErrorsString}`);
-    core.debug(`failOnWarnings: ${failOnWarningsString}`);
-    core.debug(`jmesPathQuery: ${jmesPathQuery}`);
-    core.debug(`sarifFilePath: ${sarifFilePath}`);
+    core.debug(`failOnAny: ${failOnAnyString}`)
+    core.debug(`failOnErrors: ${failOnErrorsString}`)
+    core.debug(`failOnWarnings: ${failOnWarningsString}`)
+    core.debug(`jmesPathQuery: ${jmesPathQuery}`)
+    core.debug(`sarifFilePath: ${sarifFilePath}`)
 
     // Ensure parmeters can be parsed
-    if (failOnAnyString === undefined) throw new Error("The value for `failOnAny` must be defined");
-    if (failOnErrorsString === undefined) throw new Error("The value for `failOnErrors` must be defined");
-    if (failOnWarningsString === undefined) throw new Error("The value for `failOnWarnings` must be defined");
-    if (failOnNotesString === undefined) throw new Error("The value for `failOnNotes` must be defined");
+    if (failOnAnyString === undefined)
+      throw new Error('The value for `failOnAny` must be defined')
+    if (failOnErrorsString === undefined)
+      throw new Error('The value for `failOnErrors` must be defined')
+    if (failOnWarningsString === undefined)
+      throw new Error('The value for `failOnWarnings` must be defined')
+    if (failOnNotesString === undefined)
+      throw new Error('The value for `failOnNotes` must be defined')
 
     // Ensure parmeters have the right value
-    if (failOnAnyString.toLocaleLowerCase() !== "false" && failOnAnyString.toLocaleLowerCase() !== "true") throw new Error(`Unable to parse the value '${failOnAnyString}' as a boolean for 'failOnAny'`)
-    if (failOnErrorsString.toLocaleLowerCase() !== "false" && failOnErrorsString.toLocaleLowerCase() !== "true") throw new Error(`Unable to parse the value '${failOnErrorsString}' as a boolean for 'failOnErrorsString'`)
-    if (failOnWarningsString.toLocaleLowerCase() !== "false" && failOnWarningsString.toLocaleLowerCase() !== "true") throw new Error(`Unable to parse the value '${failOnWarningsString}' as a boolean for 'failOnWarningsString'`)
-    if (failOnNotesString.toLocaleLowerCase() !== "false" && failOnNotesString.toLocaleLowerCase() !== "true") throw new Error(`Unable to parse the value '${failOnNotesString}' as a boolean for 'failOnNotesString'`)
+    if (
+      failOnAnyString.toLocaleLowerCase() !== 'false' &&
+      failOnAnyString.toLocaleLowerCase() !== 'true'
+    )
+      throw new Error(
+        `Unable to parse the value '${failOnAnyString}' as a boolean for 'failOnAny'`
+      )
+    if (
+      failOnErrorsString.toLocaleLowerCase() !== 'false' &&
+      failOnErrorsString.toLocaleLowerCase() !== 'true'
+    )
+      throw new Error(
+        `Unable to parse the value '${failOnErrorsString}' as a boolean for 'failOnErrorsString'`
+      )
+    if (
+      failOnWarningsString.toLocaleLowerCase() !== 'false' &&
+      failOnWarningsString.toLocaleLowerCase() !== 'true'
+    )
+      throw new Error(
+        `Unable to parse the value '${failOnWarningsString}' as a boolean for 'failOnWarningsString'`
+      )
+    if (
+      failOnNotesString.toLocaleLowerCase() !== 'false' &&
+      failOnNotesString.toLocaleLowerCase() !== 'true'
+    )
+      throw new Error(
+        `Unable to parse the value '${failOnNotesString}' as a boolean for 'failOnNotesString'`
+      )
 
-    let failOnAny: boolean = failOnAnyString.toLocaleLowerCase() === "true";
-    let failOnErrors: boolean = failOnErrorsString.toLocaleLowerCase() === "true";
-    let failOnWarnings: boolean = failOnWarningsString.toLocaleLowerCase() === "true";
-    let failOnNotes: boolean = failOnNotesString.toLocaleLowerCase() === "true";
+    const failOnAny: boolean = failOnAnyString.toLocaleLowerCase() === 'true'
+    const failOnErrors: boolean =
+      failOnErrorsString.toLocaleLowerCase() === 'true'
+    const failOnWarnings: boolean =
+      failOnWarningsString.toLocaleLowerCase() === 'true'
+    const failOnNotes: boolean =
+      failOnNotesString.toLocaleLowerCase() === 'true'
 
-    let sarifParser: SarifParser = new SarifParser(sarifFilePath);
+    const sarifParser: SarifParser = new SarifParser(sarifFilePath)
 
-    let errorsDetected: boolean = await sarifParser.hasErrorAlerts();
-    let warningsDetected: boolean = await sarifParser.hasWarningAlerts();
-    let notesDetected: boolean = await sarifParser.hasNoteAlerts();
+    const errorsDetected: boolean = await sarifParser.hasErrorAlerts()
+    const warningsDetected: boolean = await sarifParser.hasWarningAlerts()
+    const notesDetected: boolean = await sarifParser.hasNoteAlerts()
 
-    core.debug(`errorsDetected: ${errorsDetected}`);
+    core.debug(`errorsDetected: ${errorsDetected}`)
     core.debug(`warningsDetected: ${warningsDetected}`)
     core.debug(`notesDetected: ${notesDetected}`)
 
-    if ((failOnAny && (errorsDetected || warningsDetected || notesDetected)) ||
+    if (
+      (failOnAny && (errorsDetected || warningsDetected || notesDetected)) ||
       (failOnErrors && errorsDetected) ||
       (failOnWarnings && warningsDetected) ||
-      (failOnNotes && notesDetected)) {
-      failWorkFlow();
+      (failOnNotes && notesDetected)
+    ) {
+      core.setFailed('The JMESPath query found results')
     }
 
-    if (jmesPathQuery === undefined || jmesPathQuery.length === 0) return;
+    if (jmesPathQuery === undefined || jmesPathQuery.length === 0) return
 
-    let queryHasResults: boolean = await sarifParser.queryLogFile(jmesPathQuery);
+    const queryHasResults: boolean = await sarifParser.queryLogFile(
+      jmesPathQuery
+    )
 
     core.debug(`queryHasResults: ${queryHasResults}`)
 
-    if (queryHasResults) failWorkFlow();
-
-    // core.debug(new Date().toTimeString())
-    // await wait(parseInt(ms, 10))
-    // core.debug(new Date().toTimeString())
-
-    // core.setOutput('time', new Date().toTimeString())
+    if (queryHasResults) {
+      core.setFailed('The JMESPath query found results')
+    }
   } catch (error) {
-    let message: string = "An unknown error occured.";
+    let message = 'An unknown error occured.'
 
-    if (error instanceof Error) message = error.message;
+    if (error instanceof Error) message = error.message
 
-    core.setFailed(message);
+    core.setFailed(message)
   }
 }
 
--- src/sarif-parser.ts
+++ src/sarif-parser.ts
@@ -1,129 +1,150 @@
 import * as core from '@actions/core'
 import * as fs from 'fs'
-import { Log, ReportingDescriptor, ToolComponent } from 'sarif';
+import {Log, ReportingDescriptor, ToolComponent} from 'sarif'
 
 export class SarifParser {
-    readonly sarifLog: Log;
+  readonly sarifLog: Log
 
-    constructor(sarifFilePath: string) {
-        core.debug(`SARIF File Path: ${sarifFilePath}`);
-        if (!fs.existsSync(sarifFilePath)) {
-            throw new Error("SARIF file does not exist");
+  constructor(sarifFilePath: string) {
+    core.debug(`SARIF File Path: ${sarifFilePath}`)
+    if (!fs.existsSync(sarifFilePath)) {
+      throw new Error('SARIF file does not exist')
+    }
+
+    const buffer = fs.readFileSync(sarifFilePath, 'utf-8')
+    this.sarifLog = JSON.parse(buffer)
+  }
+
+  async queryLogFile(jmesPathQuery: string): Promise<boolean> {
+    const jmespath = require('jmespath')
+
+    core.debug(`Running query ${jmesPathQuery} ...`)
+    const result = jmespath.search(this.sarifLog, jmesPathQuery)
+
+    if (result === null || result === undefined) {
+      core.debug(`Result of query is ${result} ...`)
+      return false
+    }
+
+    return result.length > 0
+  }
+
+  async hasResults(): Promise<boolean> {
+    let hasResults = false
+
+    for (const run of this.sarifLog.runs) {
+      if (run.results !== undefined && run.results.length > 0) {
+        hasResults = true
+        break
+      }
+    }
+
+    return hasResults
+  }
+
+  async hasErrorAlerts(): Promise<boolean> {
+    return this.hasAlert('error')
+  }
+
+  async hasWarningAlerts(): Promise<boolean> {
+    return this.hasAlert('warning')
+  }
+
+  async hasNoteAlerts(): Promise<boolean> {
+    return this.hasAlert('note')
+  }
+
+  private async hasAlert(level: string): Promise<boolean> {
+    let hasAlert = false
+
+    core.debug(`Alert level: ${level} ...`)
+
+    for (const run of this.sarifLog.runs) {
+      if (run.results === null || run.results === undefined) {
+        core.debug(`Results are ${run.results}`)
+        continue
+      }
+
+      if (run.tool.extensions === null || run.tool.extensions === undefined) {
+        core.debug(`Tool Extensions are ${run.tool.extensions}`)
+        continue
+      }
+
+      for (const result of run.results) {
+        if (result.rule === null || result.rule === undefined) {
+          core.debug(`Result Rule is ${result.rule}`)
+          continue
         }
 
-        let buffer = fs.readFileSync(sarifFilePath, 'utf-8');
-        this.sarifLog = JSON.parse(buffer);
-    }
-
-    async queryLogFile(jmesPathQuery: string): Promise<boolean> {
-        var jmespath = require('jmespath');
-
-        core.debug(`Running query ${jmesPathQuery} ...`)
-        var result = jmespath.search(this.sarifLog, jmesPathQuery);
-
-        if (result === null || result === undefined) {
-            core.debug(`Result of query is ${result} ...`)
-            return false;
+        if (result.rule.index === null || result.rule.index === undefined) {
+          core.debug(`Result Rule Index is ${result.rule.index}`)
+          continue
         }
 
-        return result.length > 0;
-    }
-
-    async hasResults(): Promise<boolean> {
-        let hasResults: boolean = false;
-
-        for (let run of this.sarifLog.runs) {
-            if (run.results !== undefined && run.results.length > 0) {
-                hasResults = true;
-                break;
-            }
+        if (
+          result.rule.toolComponent === null ||
+          result.rule.toolComponent === undefined
+        ) {
+          core.debug(
+            `Result Rule Tool Component is ${result.rule.toolComponent}`
+          )
+          continue
         }
 
-        return hasResults;
-    }
-
-    async hasErrorAlerts(): Promise<boolean> {
-        return this.hasAlert('error');
-    }
-
-    async hasWarningAlerts(): Promise<boolean> {
-        return this.hasAlert('warning');
-    }
-
-    async hasNoteAlerts(): Promise<boolean> {
-        return this.hasAlert('note');
-    }
-
-    private async hasAlert(level: string): Promise<boolean> {
-        let hasAlert: boolean = false;
-
-        core.debug(`Alert level: ${level} ...`)
-
-        for (let run of this.sarifLog.runs) {
-            if (run.results === null || run.results === undefined) {
-                core.debug(`Results are ${run.results}`);
-                continue;
-            }
-
-            if (run.tool.extensions === null || run.tool.extensions === undefined) {
-                core.debug(`Tool Extensions are ${run.tool.extensions}`);
-                continue;
-            }
-
-            for (let result of run.results) {
-                if (result.rule === null || result.rule === undefined) {
-                    core.debug(`Result Rule is ${result.rule}`);
-                    continue;
-                }
-
-                if (result.rule.index === null || result.rule.index === undefined) {
-                    core.debug(`Result Rule Index is ${result.rule.index}`);
-                    continue;
-                }
-
-                if (result.rule.toolComponent === null || result.rule.toolComponent === undefined) {
-                    core.debug(`Result Rule Tool Component is ${result.rule.toolComponent}`);
-                    continue;
-                }
-
-                if (result.rule.toolComponent.index === null || result.rule.toolComponent.index === undefined) {
-                    core.debug(`Result Rule Tool Component Index is ${result.rule.toolComponent.index}`);
-                    continue;
-                }
-
-                core.debug(`Result Rule Tool Component Index: ${result.rule.toolComponent.index}`);
-
-                let toolComponent: ToolComponent = run.tool.extensions[result.rule.toolComponent.index];
-
-                if (toolComponent === null || toolComponent === undefined) {
-                    core.debug(`Tool Component is ${toolComponent}`);
-                    continue;
-                }
-
-                if (toolComponent.rules === null || toolComponent.rules === undefined) {
-                    core.debug(`Tool Component Rules are ${toolComponent.rules}`);
-                    continue;
-                }
-
-                core.debug(`Result Rule Index: ${result.rule.index}`);
-
-                let reportingDescriptor: ReportingDescriptor = toolComponent.rules[result.rule.index];
-
-                if (reportingDescriptor.defaultConfiguration === null || reportingDescriptor.defaultConfiguration === undefined) {
-                    core.debug(`Reporting Descriptor Default Configuration are ${reportingDescriptor.defaultConfiguration}`);
-                    continue;
-                }
-
-                core.debug(`Reporting Descriptor Default Configuration Level: ${reportingDescriptor.defaultConfiguration.level}`)
-                if (reportingDescriptor.defaultConfiguration.level === level) {
-                    hasAlert = true;
-                    break;
-                }
-            }
-
-            if (hasAlert) break;
+        if (
+          result.rule.toolComponent.index === null ||
+          result.rule.toolComponent.index === undefined
+        ) {
+          core.debug(
+            `Result Rule Tool Component Index is ${result.rule.toolComponent.index}`
+          )
+          continue
         }
 
-        return hasAlert;
+        core.debug(
+          `Result Rule Tool Component Index: ${result.rule.toolComponent.index}`
+        )
+
+        const toolComponent: ToolComponent =
+          run.tool.extensions[result.rule.toolComponent.index]
+
+        if (toolComponent === null || toolComponent === undefined) {
+          core.debug(`Tool Component is ${toolComponent}`)
+          continue
+        }
+
+        if (toolComponent.rules === null || toolComponent.rules === undefined) {
+          core.debug(`Tool Component Rules are ${toolComponent.rules}`)
+          continue
+        }
+
+        core.debug(`Result Rule Index: ${result.rule.index}`)
+
+        const reportingDescriptor: ReportingDescriptor =
+          toolComponent.rules[result.rule.index]
+
+        if (
+          reportingDescriptor.defaultConfiguration === null ||
+          reportingDescriptor.defaultConfiguration === undefined
+        ) {
+          core.debug(
+            `Reporting Descriptor Default Configuration are ${reportingDescriptor.defaultConfiguration}`
+          )
+          continue
+        }
+
+        core.debug(
+          `Reporting Descriptor Default Configuration Level: ${reportingDescriptor.defaultConfiguration.level}`
+        )
+        if (reportingDescriptor.defaultConfiguration.level === level) {
+          hasAlert = true
+          break
+        }
+      }
+
+      if (hasAlert) break
     }
+
+    return hasAlert
+  }
 }
